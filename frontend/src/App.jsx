0d68be1938c89a85d489af5b954627345d545629
bhargavkundrapu <kundrapubhargav@gmail.com>
2025-12-16 23:59:10 +0530

    Fix vercel deployment config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://crre.onrender.com/"
 function App() {
    getCurrentUser()
    getSuggestedUsers()
